d8417a899e19582884927d2a6b9cfed915eaa90b
Lara Balsamello <lara.balsamello@gmail.com>
2020-02-28 17:19:04 -0300

    run eject on project for setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Layout from './Containers/Layout';
+import { Route, Redirect } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <BrowserRouter>
+        <Redirect from="**" to="/home" />
+        <Route exact={false} path="/home" component={Layout}></Route>
+      </BrowserRouter>
     </div>
   );
 }
